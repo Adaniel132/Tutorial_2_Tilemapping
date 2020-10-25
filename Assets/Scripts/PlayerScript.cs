5d2ab73179a5e9eb97ab6e96f52f46fa848ed9bf
Azaan Daniel <azaandaniel132@knights.ucf.edu>
2020-10-25 19:39:26 -0400

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -10,12 +10,36 @@ public class PlayerScript : MonoBehaviour
     public float speed;
     public Text score;
     private int scoreValue = 0;
+    public Text winText;
+    public Text lives;
+    private int livesValue = 3;
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    public AudioClip musicClipThree;
+    Animator anim;
+    private bool facingRight = true;
+    private bool isOnGround;
+    public Transform groundcheck;
+    public float checkRadius;
+    public LayerMask allGround;
+    private bool isJumping;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        winText.text = "";
+        lives.text = livesValue.ToString();
+        anim = GetComponent<Animator>();
+
+    musicSource.clip = musicClipOne;
+          musicSource.Play();
+
+
     }
 
     // Update is called once per frame
@@ -23,12 +47,63 @@ public class PlayerScript : MonoBehaviour
     {
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
+       
 
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+       
+        isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
 
-        if (Input.GetKey("escape"))
+         {
+       
+        if (Input.GetKeyDown(KeyCode.D))
+        
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+       
+        {
+            anim.SetInteger("State", 0);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.A))
+        
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        
+        {
+            anim.SetInteger("State", 2);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (facingRight == false && hozMovement > 0)
+            {
+             Flip();
+            }
+        else if (facingRight == true && hozMovement < 0)
+            {
+             Flip();
+            }
+
+       if (Input.GetKey("escape"))
         {
         Application.Quit();
+            }
         }
     }
 
@@ -39,18 +114,52 @@ public class PlayerScript : MonoBehaviour
              scoreValue += 1;
              score.text = scoreValue.ToString();
              Destroy(collision.collider.gameObject);
-         }
+        if (scoreValue == 4)
+        {
+            livesValue = 3;
+            lives.text = livesValue.ToString();
+            transform.position = new Vector3(77.17062f, 3.911461f, 0.0f);
+        }
+        if (scoreValue >= 8)
+        {
+            winText.text = "You Win! Game created by Azaan Daniel"; 
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+            }   
+        }
     }
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Ground")
+        if (collision.collider.tag == "Ground" && isOnGround)
         {
             if (Input.GetKey(KeyCode.W))
             {
                 rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
             }
+        {
+    }
+        }
+        if (collision.collider.tag == "Enemy") 
+        {
+             livesValue -= 1;
+             lives.text = livesValue.ToString();
+            Destroy(collision.collider.gameObject);
+        if (livesValue <= 0) 
+        {
+            winText.text = "You Lose";
+            musicSource.clip = musicClipThree;
+            musicSource.Play(); 
+        }
         }
     }
+
+    void Flip()
+        {
+     facingRight = !facingRight;
+     Vector2 Scaler = transform.localScale;
+     Scaler.x = Scaler.x * -1;
+     transform.localScale = Scaler;
+        }
 }
 
 
